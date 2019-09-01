5589a48520173595d4b8fda5b08fe08ffaf43384
Anthony A <asanderson2009@gmail.com>
2019-09-01 12:04:41 -0400

    fixed css styling for background to be in body. Changed layout to include dashboard page for user, changed dabatase init structure to assist with the verification aspect. Renamed some files in order to more easily identify what they are when navigating them.

--- models/example.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    user: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  });
-  return User;
-};
