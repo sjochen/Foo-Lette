fc184bf113417e9cf49388201bc60f7231b053e1
Anthony A <asanderson2009@gmail.com>
2019-09-03 21:38:00 -0400

    still tweaking

--- models/userDB.js
+++ models/userDB.js
@@ -33,11 +33,6 @@ module.exports = function (sequelize, DataTypes) {
     return bcrypt.compareSync(password, this.password);
   };
 
-  // create all the defined tables in the specified database.
-sequelize.sync()
-.then(() => console.log('users table has been successfully created, if one doesn\'t exist'))
-.catch(error => console.log('This error occured', error));
-
 
   return User;
 
