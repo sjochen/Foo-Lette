c02e83663ffbb2d44e1a217bb9a8208e0118bb7d
Curtis Jochen <curtis.jochen@gmail.com>
2019-09-07 09:04:54 -0400

    Added API call to return a random restraunt name based on location

--- public/js/index.js
+++ public/js/index.js
@@ -9,6 +9,27 @@ $(document).ready(function(){
       type: "GET"
     }).then(function(data){
       console.log(data);
+
+      var pick = Math.floor(Math.random() * data.length);
+      console.log(pick)
+      var name = data[pick].name;
+      console.log("this is results for name: " + name)
+
+          // var div = $('<div>');
+          // var map = $('<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/search?' + name + '&key=AIzaSyAqGzyDwZXtg3v10ZpY4cPVRyHDVHU7T0w" allowfullscreen>');
+
+          //  div.append(map);
+          //  $('.randomChoice').append(div);
+          //initMap(lat, long, data)
+        $('.randomChoice').empty();
+         var head = $('<h1>')
+
+         head.append(name);
+         $('.randomChoice').append(head);
+
+
+     
+
     })
     
 
@@ -26,102 +47,3 @@ $(document).ready(function(){
 
 
 
-// // Get references to page elements
-// var $userText = $("#example-text");
-// var $exampleDescription = $("#example-description");
-// var $submitBtn = $("#submit");
-// var $exampleList = $("#example-list");
-
-// // The API object contains methods for each kind of request we'll make
-// var API = {
-//   saveExample: function(example) {
-//     return $.ajax({
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//       type: "POST",
-//       url: "api/examples",
-//       data: JSON.stringify(example)
-//     });
-//   },
-//   getExamples: function() {
-//     return $.ajax({
-//       url: "api/examples",
-//       type: "GET"
-//     });
-//   },
-//   deleteExample: function(id) {
-//     return $.ajax({
-//       url: "api/examples/" + id,
-//       type: "DELETE"
-//     });
-//   }
-// };
-
-// // refreshExamples gets new examples from the db and repopulates the list
-// var refreshExamples = function() {
-//   API.getExamples().then(function(data) {
-//     var $examples = data.map(function(example) {
-//       var $a = $("<a>")
-//         .text(example.text)
-//         .attr("href", "/example/" + example.id);
-
-//       var $li = $("<li>")
-//         .attr({
-//           class: "list-group-item",
-//           "data-id": example.id
-//         })
-//         .append($a);
-
-//       var $button = $("<button>")
-//         .addClass("btn btn-danger float-right delete")
-//         .text("ｘ");
-
-//       $li.append($button);
-
-//       return $li;
-//     });
-
-//     $exampleList.empty();
-//     $exampleList.append($examples);
-//   });
-// };
-
-// // handleFormSubmit is called whenever we submit a new example
-// // Save the new example to the db and refresh the list
-// var handleLoginSubmit = function(event) {
-//   event.preventDefault();
-
-//   var userToFind = {
-//     user: $exampleText.val().trim(),
-//     description: $exampleDescription.val().trim()
-//   };
-
-//   if (!(example.text && example.description)) {
-//     alert("You must enter an example text and description!");
-//     return;
-//   }
-
-//   API.saveExample(example).then(function() {
-//     refreshExamples();
-//   });
-
-//   $exampleText.val("");
-//   $exampleDescription.val("");
-// };
-
-// // handleDeleteBtnClick is called when an example's delete button is clicked
-// // Remove the example from the db and refresh the list
-// var handleDeleteBtnClick = function() {
-//   var idToDelete = $(this)
-//     .parent()
-//     .attr("data-id");
-
-//   API.deleteExample(idToDelete).then(function() {
-//     refreshExamples();
-//   });
-// };
-
-// // Add event listeners to the submit and delete buttons
-// $submitBtn.on("click", handleFormSubmit);
-// $exampleList.on("click", ".delete", handleDeleteBtnClick);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express.Router();
 var axios = require('axios');
 
-var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=" + process.env.API_KEY;
+var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=	37.541290,-77.434769&radius=15000&type=restaurant&&key=" + process.env.API_KEY;
 
 module.exports = function(app){
 
@@ -10,7 +10,7 @@ module.exports = function(app){
     
     axios.get(url).then(function(response){
       
-      res.json(response.data)
+      res.json(response.data.results)
 
     });
 
@@ -20,15 +20,3 @@ module.exports = function(app){
 
 
 };
-
-
-
-
-
-
-// app.get("/api/posts/", function(req, res) {
-//   db.Post.findAll({})
-//     .then(function(dbPost) {
-//       res.json(dbPost);
-//     });
-// });
